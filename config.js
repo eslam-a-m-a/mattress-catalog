8433001c069f933ce0df93aa0f87b2f909d97793
eslam-a-m-a <ea181415@gmail.com>
2026-08-12 21:08:31 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-// Supabase configuration
-// Project URL & anon public key
-
-window.SUPABASE_URL="https://whimsicalwharfwsebrnb.supabase.co";
-
-window.SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXN6aG9haHZmc2JtZWRqcmJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MTUwODQsImV4cCI6MjEwMjA5MTA4NH0.NRdVz5Gpmsoj-VGhPVWTMwqSvAvC5Vg-8o1BEmYPJHI";
+// ضع القيم من Supabase هنا
+const SUPABASE_URL = "ضع_رابط_Supabase_هنا";
+const SUPABASE_PUBLISHABLE_KEY = "ضع_Publishable_Key_هنا";
