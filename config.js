b621f8548e111d1eceb96c06973c8c58307f12f4
eslam-a-m-a <ea181415@gmail.com>
2026-08-12 19:07:04 +0300

    Update Supabase configuration values
    Updated Supabase URL and anon key in config.js.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase configuration
 // ضع هنا الـ Project URL والـ anon public key من Supabase.
-window.SUPABASE_URL = 'https://wmiszhoahvfsbmedjrbm.supabase.co';
-window.SUPABASE_ANON_KEY = 'ضع_هنا_مفتاح_anon_العام';
+window.SUPABASE_URL =https://wmiszhoahvfsbmedjrbm.supabase.co';
+window.SUPABASE_ANON_KEY =sb_publishable_fFclLm6Z2fdUTYxD_AAC5w_icPdd_n5';
