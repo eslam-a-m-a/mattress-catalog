ad94db6cf4a6aaf2b770ec32c0ab2590f203efa8
eslam-a-m-a <ea181415@gmail.com>
2026-08-12 20:35:32 +0300

    Update Supabase configuration in config.js
    Updated Supabase URL and anon public key in config.js.

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
-// Supabase configuration
-// ضع هنا الـ Project URL والـ anon public key من Supabase.
-window.SUPABASE_URL=https://wmiszhoahvfsbmedjrbm.supabase.co';
 window.SUPABASE_URL = "https://whimsicalwharfwsebrnb.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtaXN6aG9haHZmc2JtZWRqcmJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1MTUwODQsImV4cCI6MjEwMjA5MTA4NH0.NRdVz5Gpmsoj-VGhPVWTMwqSvAvC5Vg-8o1BEmYPJHI";
