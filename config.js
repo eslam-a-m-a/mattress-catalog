fcf6ee62e1fa4cc1b0fa14240c52bab8868c35d1
eslam-a-m-a <ea181415@gmail.com>
2026-08-12 21:11:44 +0300

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ضع القيم من Supabase هنا
-const SUPABASE_URL = "ضع_رابط_Supabase_هنا";
-const SUPABASE_PUBLISHABLE_KEY = "ضع_Publishable_Key_هنا";
+const SUPABASE_URL = "https://wmiszhoahvfsbmedjrbm.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_fFclLm6Z2fdUTYxD_AAC5w_icPdd_n5";
